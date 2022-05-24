48bd02a3dbec682a9141844a7d6334e5f31fd678
Mark Estrada <h.mark.estrada@gmail.com>
2022-05-24 18:02:06 -0500

    Create script.js
    JS for background features.

---
+++ assets/javascript/script.js
@@ -0,0 +1,5 @@
+//https://openweathermap.org/
+var APIweather = "e0c60dfc5ac6ce8b4f611d75a789698d";
+
+//http://api.ipstack.com/134.201.250.155? access_key = 1920c3c54cdf1d5baee79d3ca5f08853
+var APIlocation = "1920c3c54cdf1d5baee79d3ca5f08853";
